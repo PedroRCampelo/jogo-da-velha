328886aba18322b8fbaf3566bb32428fc57d91f0
PedroRCampelo <PedroRCampelo>
2022-03-01 20:38:09 -0300

    Adicionando scripts para actions e players

---
+++ scripts/interface.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.addEventListener('click', handleClick);
+  }) 
+
+
+});
+
+let handleClick = function(event){
+  let square = event.target;
+  let position = square.id;
+
+  handleMove(position);
+  updateSquares();
+}
+
+let updateSquares = function(){
+
+  let squares = document.querySelectorAll('.square');
+
+  squares.forEach((square) => {
+    let position = square.id;
+    let symbol = board[position];
+
+    if (symbol != ''){
+      square.innerHTML = `<div class = '${symbol}' </div>`;
+    }
+  }) 
+
+}
